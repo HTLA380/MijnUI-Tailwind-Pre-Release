55b13c88ab6c669bf1c9e8d7fa1e8bd2a7043b96
super-SH <dev.swam.htet@gmail.com>
2024-03-21 22:31:15 +0630

    add button components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
-        width:{
-          '57px':'57px',
-          '518px':'518px',
-          '28px':'28px',
-          
-        },
-        height:{
-          '80px':'80px',
-          '28px':'28px',
-        }
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      width: {
+        '57px': '57px',
+        '518px': '518px',
+        '28px': '28px',
+      },
+      height: {
+        '80px': '80px',
+        '28px': '28px',
+      },
+      outlineOffset: {
+        'inner-1': '-1px',
+        'inner-2': '-2px',
+        'inner-4': '-4px',
       },
     },
-    plugins: [],
-  }
-  
-  
+  },
+  plugins: [],
+};
