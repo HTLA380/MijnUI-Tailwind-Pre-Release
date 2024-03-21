0c10de349523ea2b2534768776d6475498a58c7b
waiyanphoneaant <wypa07@gmail.com>
2024-03-21 20:45:14 +0700

    tailwind init'

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        width:{
+          '57px':'57px',
+          '518px':'518px',
+          '28px':'28px',
+          
+        },
+        height:{
+          '80px':'80px',
+          '28px':'28px',
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
