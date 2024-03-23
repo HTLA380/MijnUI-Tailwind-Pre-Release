37228e71a08f96946097cdc7f18a377512047d51
Htet Aung Lin <htetlin099798@gmail.com>
2024-03-23 16:02:15 +0630

    add accordion component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      inter: ["inter", "sans-serif"],
+    },
     extend: {
       width: {
-        '28px': '28px',
-        '57px': '57px',
-        '152px': '152px',
-        '518px': '518px',
+        "10px": "0.625rem",
+        "28px": "1.75rem",
+        "57px": "3.5625rem",
+        "152px": "152px",
+        "512px": "32rem",
+        "518px": "32.375rem",
+      },
+      maxWidth: {
+        "512px": "32rem",
       },
       height: {
-        '28px': '28px',
-        '80px': '80px',
+        "28px": "1.75rem",
+        "80px": "5rem",
       },
       outlineOffset: {
-        'inner-1': '-1px',
-        'inner-2': '-2px',
-        'inner-4': '-4px',
+        "inner-1": "-1px",
+        "inner-2": "-2px",
+        "inner-4": "-4px",
       },
     },
   },
