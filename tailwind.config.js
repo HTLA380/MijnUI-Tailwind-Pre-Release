8c0ad04044a8c7fb2e19c12d89165ab976451afe
super-SH <dev.swam.htet@gmail.com>
2024-03-29 21:42:35 +0630

    add top bar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     fontFamily: {
-      inter: ["inter", "sans-serif"],
+      inter: ['inter', 'sans-serif'],
     },
     extend: {
       width: {
-        "10px": "0.625rem",
-        "28px": "1.75rem",
-        "57px": "3.5625rem",
-        "152px": "152px",
-        "518px": "32.375rem",
+        '10px': '0.625rem',
+        '28px': '1.75rem',
+        '57px': '3.5625rem',
+        '152px': '152px',
+        '518px': '32.375rem',
+        '1440px': '90rem',
       },
       height: {
-        "28px": "1.75rem",
-        "80px": "5rem",
+        '28px': '1.75rem',
+        '80px': '5rem',
       },
       outlineOffset: {
-        "inner-1": "-1px",
-        "inner-2": "-2px",
-        "inner-4": "-4px",
+        'inner-1': '-1px',
+        'inner-2': '-2px',
+        'inner-4': '-4px',
       },
     },
   },
