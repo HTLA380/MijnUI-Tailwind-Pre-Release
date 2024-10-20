44c6b414458c327925e1dbabed09d0a00aa54b55
HTLA380 <htetlin099798@gmail.com>
2024-10-20 17:32:48 +0630

    feat(docs): add Buttons page

--- mijnui-react/ui/button.tsx
+++ mijnui-react/ui/button.tsx
@@ -7,123 +7,35 @@ import { LuLoader2 } from "react-icons/lu"
 
 const buttonStyles = cva(
   [
-    "inline-flex items-center justify-center gap-1 transition-colors <duration-300></duration-300> ease-in-out active:brightness-90 text-sm disabled:pointer-events-none disabled:brightness-75 disabled:opacity-80",
+    "inline-flex items-center justify-center gap-1 transition-colors duration-150 active:brightness-95 text-sm disabled:pointer-events-none disabled:brightness-75 disabled:opacity-80 ",
   ],
   {
     variants: {
       variant: {
-        primary: "",
-        secondary: "",
-        surface: "",
-        danger: "",
+        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/90",
+        outline:
+          "border border-main-border hover:bg-accent hover:text-accent-foreground",
+        danger: "bg-danger text-danger-filled-foreground hover:bg-danger/90",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        surface:
+          "bg-surface text-surface-foreground hover:bg-surface/90 shadow-sm",
       },
       radius: {
-        none: "rounded-none",
         sm: "rounded-sm",
         md: "rounded-md",
         lg: "rounded-lg",
         full: "rounded-full",
       },
       size: {
-        sm: "h-9 px-3",
+        sm: "h-8 px-2.5",
         md: "h-10 px-3",
         lg: "h-11 px-8",
         icon: "h-10 w-10",
       },
-      appearance: {
-        filled: "",
-        outline: "border border-current",
-        text: "bg-transparent",
-      },
     },
-    compoundVariants: [
-      {
-        variant: "primary",
-        appearance: "outline",
-        className:
-          "hover:bg-primary hover:text-primary-foreground border border-primary text-primary",
-      },
-      {
-        variant: "secondary",
-        appearance: "outline",
-        className:
-          "hover:bg-secondary hover:text-secondary-foreground border border-secondary text-secondary",
-      },
-      {
-        variant: "surface",
-        appearance: "outline",
-        className:
-          "hover:bg-surface hover:text-surface-foreground border border-surface-foreground text-surface-foreground",
-      },
-      {
-        variant: "danger",
-        appearance: "outline",
-        className:
-          "hover:bg-red-500 hover:text-white border border-red-500 text-red-500",
-      },
-
-      {
-        variant: "primary",
-        appearance: "outline",
-        className:
-          "hover:bg-primary hover:text-primary-foreground border border-primary text-primary",
-      },
-
-      {
-        variant: "primary",
-        appearance: "text",
-        className:
-          "hover:bg-primary hover:text-primary-foreground text-primary",
-      },
-      {
-        variant: "secondary",
-        appearance: "text",
-        className:
-          "hover:bg-secondary hover:text-secondary-foreground text-secondary",
-      },
-      {
-        variant: "surface",
-        appearance: "text",
-        className:
-          "hover:bg-surface hover:text-surface-foreground text-surface-foreground",
-      },
-      {
-        variant: "danger",
-        appearance: "text",
-        className: "hover:bg-red-500 hover:text-white text-red-500",
-      },
-
-      {
-        variant: "primary",
-        appearance: "outline",
-        className:
-          "hover:bg-primary hover:text-primary-foreground border border-primary text-primary",
-      },
-
-      {
-        variant: "primary",
-        appearance: "filled",
-        className: "bg-primary text-primary-foreground hover:bg-primary/90",
-      },
-      {
-        variant: "secondary",
-        appearance: "filled",
-        className:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/90",
-      },
-      {
-        variant: "danger",
-        appearance: "filled",
-        className: "bg-danger text-danger-filled-foreground hover:bg-danger/80",
-      },
-      {
-        variant: "surface",
-        appearance: "filled",
-        className: "bg-surface border text-surface-foreground hover:bg-accent",
-      },
-    ],
     defaultVariants: {
-      appearance: "filled",
       variant: "primary",
       radius: "md",
       size: "md",
@@ -143,7 +55,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       unstyled,
       className,
       variant,
-      appearance,
       size,
       radius,
       loading,
@@ -160,7 +71,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Component
         className={applyUnstyled(
           unstyled,
-          buttonStyles({ variant, appearance, size, radius }),
+          buttonStyles({ variant, size, radius }),
           className,
         )}
         ref={ref}
