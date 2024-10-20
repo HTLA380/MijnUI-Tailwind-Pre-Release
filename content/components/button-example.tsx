b8debe25089ec769d3e073029f0652049c59807a
HTLA380 <htetlin099798@gmail.com>
2024-10-20 18:08:44 +0630

    chore(docs): update Buttons page

---
+++ content/components/button-example.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { Button } from "@/mijnui-react/ui/button"
+
+export const ButtonVariants = () => {
+  return (
+    <div className="w-full p-5 flex border rounded-lg items-center gap-5">
+      <Button variant={"primary"}>Button</Button>
+      <Button variant={"secondary"}>Button</Button>
+      <Button variant={"surface"}>Button</Button>
+      <Button variant={"ghost"}>Button</Button>
+      <Button variant={"outline"}>Button</Button>
+      <Button variant={"danger"}>Button</Button>
+    </div>
+  )
+}
+
+export const ButtonSizes = () => {
+  return (
+    <div className="w-full p-5 flex border rounded-lg items-center gap-5">
+      <Button size={"sm"}>Button</Button>
+      <Button size={"md"}>Button</Button>
+      <Button size={"lg"}>Button</Button>
+      <Button size={"icon"}>B</Button>
+    </div>
+  )
+}
+
+export const ButtonRadius = () => {
+  return (
+    <div className="w-full p-5 flex border rounded-lg items-center gap-5">
+      <Button radius={"sm"}>Button</Button>
+      <Button radius={"md"}>Button</Button>
+      <Button radius={"lg"}>Button</Button>
+      <Button radius={"full"}>Button</Button>
+    </div>
+  )
+}
