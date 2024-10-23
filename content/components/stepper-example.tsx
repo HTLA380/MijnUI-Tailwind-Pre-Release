fef236e7659c05fe9b6b6044bc3ffe4db6c6e640
HTLA380 <htetlin099798@gmail.com>
2024-10-23 20:10:24 +0630

    docs: fix responsiveness

--- content/components/stepper-example.tsx
+++ content/components/stepper-example.tsx
@@ -2,38 +2,38 @@ import React from "react"
 
 export const DefaultStepper = () => {
   return (
-    <div className="not-prose flex min-h-80 w-full items-center justify-center gap-5 rounded-lg border p-5">
-      <div className="flex flex-col items-center gap-2 sm:flex-row">
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border border-foreground bg-surface text-sm">
+    <div className="not-prose flex min-h-80 w-full items-center justify-center gap-5 overflow-auto rounded-lg border p-5">
+      <div className="flex w-full max-w-xl flex-col items-center justify-center gap-2 sm:flex-row">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border border-foreground bg-surface text-sm">
             1
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             2
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             3
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             4
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
         <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             5
           </button>
         </div>
@@ -44,38 +44,38 @@ export const DefaultStepper = () => {
 
 export const ActiveStepper = () => {
   return (
-    <div className="not-prose flex min-h-80 w-full items-center justify-center gap-5 rounded-lg border p-5">
-      <div className="flex flex-col items-center gap-2 sm:flex-row">
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border border-foreground bg-foreground text-sm text-background">
+    <div className="not-prose flex min-h-80 w-full items-center justify-center gap-5 overflow-auto rounded-lg border p-5">
+      <div className="flex w-full max-w-xl flex-col items-center justify-center gap-2 sm:flex-row">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border border-foreground bg-foreground text-sm text-background">
             1
           </button>
-          <div className="h-8 w-px bg-foreground sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-foreground sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border border-foreground bg-surface text-sm">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border border-foreground bg-surface text-sm">
             2
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             3
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+        <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             4
           </button>
-          <div className="h-8 w-px bg-border sm:h-px sm:w-20"></div>
+          <div className="h-8 w-px bg-border sm:h-px sm:w-full sm:max-w-40"></div>
         </div>
 
         <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full border text-sm text-muted-foreground">
+          <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full border text-sm text-muted-foreground">
             5
           </button>
         </div>
