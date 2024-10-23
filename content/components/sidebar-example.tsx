6cc97b3466e481e88f66078c6913f26eb454cd41
HTLA380 <htetlin099798@gmail.com>
2024-10-23 18:45:36 +0630

    docs: add navbar page and update components

--- content/components/sidebar-example.tsx
+++ content/components/sidebar-example.tsx
@@ -3,8 +3,8 @@ import { MdOutlineLocationOn } from "react-icons/md"
 
 export const DefaultSidebar = () => {
   return (
-    <div className="not-prose relative flex h-[650px] w-full items-center justify-center gap-5 overflow-hidden rounded-lg border p-5">
-      <aside className="absolute inset-y-0 left-0 w-52 space-y-2 overflow-y-auto border-r border-border bg-surface px-3 pb-4 pt-2 shadow-sm">
+    <div className="not-prose relative flex h-[450px] w-full gap-5 overflow-y-auto overflow-x-hidden rounded-lg border sm:h-[650px]">
+      <aside className="sticky left-0 top-0 h-full w-52 shrink-0 space-y-2 overflow-y-auto border-r border-border bg-surface px-3 pb-4 pt-2 shadow-sm">
         <div className="flex items-center gap-2">
           <button className="disabled:text-muted/75-foreground inline-flex h-10 w-10 items-center justify-center gap-1 rounded-md border text-sm hover:bg-accent hover:text-accent-foreground disabled:bg-muted/75">
             <svg
@@ -211,14 +211,84 @@ export const DefaultSidebar = () => {
           </a>
         </ul>
       </aside>
+
+      <div className="h-screen w-full p-5">
+        <div className="mx-auto max-w-lg">
+          {" "}
+          <div role="status" className="my-7 animate-pulse">
+            {" "}
+            <div className="mb-4 h-2.5 w-48 rounded-full bg-muted"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[380px] rounded-full bg-accent"></div>
+            <div className="h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="mb-7 max-w-lg animate-pulse">
+            {" "}
+            <div className="flex h-48 w-full items-center justify-center rounded bg-muted">
+              {" "}
+              <svg
+                className="h-12 w-12 text-accent"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 640 512">
+                <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path>
+              </svg>{" "}
+            </div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="my-6 animate-pulse">
+            {" "}
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[380px] rounded-full bg-accent"></div>
+            <div className="h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="my-6 animate-pulse">
+            {" "}
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="mb-6 mt-7 animate-pulse">
+            {" "}
+            <div className="mb-4 h-2.5 w-48 rounded-full bg-muted"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[380px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="my-6 animate-pulse">
+            {" "}
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export const SidebarCollapsibleActive = () => {
   return (
-    <div className="not-prose relative flex h-[650px] w-full items-center justify-center gap-5 overflow-hidden rounded-lg border p-5">
-      <aside className="absolute inset-y-0 left-0 w-52 space-y-2 overflow-y-auto border-r bg-surface px-3 pb-4 pt-2 shadow-sm">
+    <div className="not-prose relative flex h-[450px] w-full items-center gap-5 overflow-y-auto overflow-x-hidden rounded-lg border sm:h-[650px]">
+      <aside className="sticky left-0 top-0 h-full w-52 shrink-0 space-y-2 overflow-y-auto border-r bg-surface px-3 pb-4 pt-2 shadow-sm">
         <div className="flex items-center gap-2">
           <button className="disabled:text-muted/75-foreground inline-flex h-10 w-10 items-center justify-center gap-1 rounded-md border text-sm hover:bg-accent hover:text-accent-foreground disabled:bg-muted/75">
             <svg
@@ -448,6 +518,76 @@ export const SidebarCollapsibleActive = () => {
           </a>
         </ul>
       </aside>
+
+      <div className="h-screen w-full p-5">
+        <div className="mx-auto max-w-lg">
+          {" "}
+          <div role="status" className="my-7 animate-pulse">
+            {" "}
+            <div className="mb-4 h-2.5 w-48 rounded-full bg-muted"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[380px] rounded-full bg-accent"></div>
+            <div className="h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="mb-7 max-w-lg animate-pulse">
+            {" "}
+            <div className="flex h-48 w-full items-center justify-center rounded bg-muted">
+              {" "}
+              <svg
+                className="h-12 w-12 text-accent"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 640 512">
+                <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path>
+              </svg>{" "}
+            </div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="my-6 animate-pulse">
+            {" "}
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[380px] rounded-full bg-accent"></div>
+            <div className="h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="my-6 animate-pulse">
+            {" "}
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="mb-6 mt-7 animate-pulse">
+            {" "}
+            <div className="mb-4 h-2.5 w-48 rounded-full bg-muted"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[380px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[500px] rounded-full bg-accent"></div>
+            <div className="h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+          <div role="status" className="my-6 animate-pulse">
+            {" "}
+            <div className="mb-2.5 h-2 max-w-[460px] rounded-full bg-accent"></div>
+            <div className="mb-2.5 h-2 max-w-[450px] rounded-full bg-accent"></div>
+            <span className="sr-only">Loading...</span>{" "}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
